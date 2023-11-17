0b693a045642b69f5aa040c8b7703327cc694402
Igwilo Ethel-Doris <etheldorisamarachi@gmail.com>
2023-11-17 21:19:32 +0100

    updated

---
+++ stringtimes.js
@@ -0,0 +1,14 @@
+Warmup-2 -- stringTimes
+Given a string and a non-negative int n, return a larger string that is n copies of the original string.
+
+Examples
+
+stringTimes('Hi', 2) → HiHi
+stringTimes('Hi', 3) → HiHiHi
+stringTimes('Hi', 1) → Hi
+
+function stringTimes(str, n){
+    if(typeof(str) === "string"){
+    return str.repeat(n)
+    }
+  }console.log(stringTimes('Hi', 2));
